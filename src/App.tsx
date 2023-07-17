41fe2c2ee2c6a9f3bf51f3307f0fde0152bdb1e5
TurovaE <ekaterina.turova@mail.ru>
2023-07-18 02:54:44 +0700

    test env variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [res, setRes] = useState({ status: "none" });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const BASE_URL = import.meta.env.VITE_URL;
+      console.log(BASE_URL);
+      try {
+        const response = await fetch(BASE_URL + "/error");
+        console.log(response);
+        // if (!response.ok) {
+        //   throw new Error("error load");
+        // }
+        const data = await response.json();
+        setRes(data);
+        console.log(data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
@@ -28,8 +50,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p>{res.status}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
